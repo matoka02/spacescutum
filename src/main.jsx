a998d9f0b318c70e02e8edfeaa6d47fc69a42a98
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-06-07 13:32:24 +0300

    import router in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/spacescutum/'>
+    <BrowserRouter basename='/spacescutum/'> 
       <App />
     </BrowserRouter>
   </React.StrictMode>,
